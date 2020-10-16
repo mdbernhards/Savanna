2b4415a649f4f6b6f72894c0b34ed2971b1afc8a
Martins Davis Bernhards <49512684+mdbernhards@users.noreply.github.com>
2020-10-16 11:14:17 +0300

    addedproject

---
+++ Savanna/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Savanna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
